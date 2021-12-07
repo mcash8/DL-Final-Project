4fd36ca5a18e7ddaaf5c8e7e6a87d67d8019bda2
Martha Cash <63068410+mcash8@users.noreply.github.com>
2021-12-07 15:08:52 -0600

    Final Implementation
    Included trained model links.

--- test_2.py
+++ test_2.py
@@ -10,6 +10,21 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import matplotlib.ticker as ticker
 import numpy as np
+import time
+import math
+
+#import urllib 
+import urllib.request
+import pickle
+
+'''
+CSC 7343 Deep Learning Final Project: Martha Cash and Emmanual Akoja
+Model is adapted from: https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
+'''
+
+filename = sys.argv[1]
+
+Train = True if filename == 'data_train.txt' else False
 
 def openFile(file): 
     '''
@@ -29,22 +44,23 @@ def openFile(file):
                 input.append(line.strip('\n'))
     return input, output
 
-input, output = openFile('data_train.txt')
-
-max_length = len(max(output, key=len))
-
-predictInput, _ = openFile('data_predict.txt')
+if Train:
+    input, output = openFile(filename)
+    max_length = len(max(output, key=len))
+else:
+    predictInput, _ = openFile(filename)
+    max_length = 24
+    
 
 sos_tkn = 0
 eos_tkn = 1
 
 class Sequence:
     '''
-    Class to help make a dictionary. Each word from the input or target array is split into letters
-    and then added to the dictionary. Each letter is added to an index
+    Class to help make a dictionary. Each word from the input or target is split into letters
+    and then added to the dictionary. Each letter is added to an index to keep track of frequency of letter. 
     '''
     def __init__(self): 
-    #initialize containers to hold the letters and corresponding index
         self.letter2index = {}
         self.letter2count = {}
         self.index2letter = {0: "SOS", 1: "EOS"}
@@ -75,7 +91,7 @@ class Sequence:
 def pairData(input, output): 
 
     '''
-    Description: Encode input and output sequences 
+    Description: Pass input and output sequence to Sequence class. Create pairs of sequences. 
     '''
 
     source = Sequence()
@@ -93,6 +109,9 @@ def pairData(input, output):
     return source, target, pairs
 
 def loadPredict(input):
+    '''
+    Description: Same as pairData function but used for data_predict.txt
+    '''
     predict = Sequence()
 
     for i in range(len(input)):
@@ -100,39 +119,47 @@ def loadPredict(input):
     
     return predict
 
-source, target, pairs = pairData(input, output)
-print(random.choice(pairs))
-print(source.n_letters)
-print(target.n_letters)
+if Train:
+    source, target, pairs = pairData(input, output)
+else:
+    predict = loadPredict(predictInput)
 
-predict = loadPredict(predictInput)
-
-def indexesFromWord(Sequence, word):
+def indexesFromSequence(Sequence, word):
+    '''
+    Get indexes of letters in sequences
+    '''
     return [Sequence.letter2index[letter] for letter in word]
 
-def tensorFromWord(Sequence, word):
-    indexes = indexesFromWord(Sequence, word)
+def tensorFromSequence(Sequence, word):
+    '''
+    Convert sequnece to indexes and cast to tensor
+    '''
+    indexes = indexesFromSequence(Sequence, word)
     indexes.append(eos_tkn)
     return torch.tensor(indexes, dtype=torch.long, device=device).view(-1,1)
 
 def tensorsFromPair(pair):
-    input_tensor = tensorFromWord(source, pair[0])
-    target_tensor = tensorFromWord(target, pair[1])
+    '''
+    Convert pair to a tensor
+    '''
+    input_tensor = tensorFromSequence(source, pair[0])
+    target_tensor = tensorFromSequence(target, pair[1])
     return (input_tensor, target_tensor)
 
-def tensorsForPredict(input): 
-    predict_tensor = tensorFromWord(predict, predictInput)
+def tensorsForPredict(input):
+    '''
+    Convert predicted sequence to a tensor
+    ''' 
+    predict_tensor = tensorFromSequence(predict, predictInput)
     return predict_tensor
 
 '''
 The Model
 '''
-
 class Encoder(nn.Module): 
     def __init__(self, input_size, hidden_size):
         super(Encoder, self).__init__()
         self.hidden_size = hidden_size
-
         self.embedding = nn.Embedding(input_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
 
@@ -145,14 +172,13 @@ class Encoder(nn.Module):
     def initHidden(self): 
         return torch.zeros(1,1, self.hidden_size, device=device)
 
-class AttnDecoder(nn.Module):
+class Decoder(nn.Module):
     def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=max_length):
-        super(AttnDecoder, self).__init__()
+        super(Decoder, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.dropout_p = dropout_p
         self.max_length = max_length
-
         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
         self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
@@ -164,10 +190,8 @@ class AttnDecoder(nn.Module):
         embedded = self.embedding(input).view(1, 1, -1)
         embedded = self.dropout(embedded)
 
-        attn_weights = F.softmax(
-            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
-        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
-                                encoder_outputs.unsqueeze(0))
+        attn_weights = F.softmax(self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),encoder_outputs.unsqueeze(0))
 
         output = torch.cat((embedded[0], attn_applied[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
@@ -183,8 +207,11 @@ class AttnDecoder(nn.Module):
 
 teacher_forcing_ratio = 0.5
 
-
 def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=max_length):
+    '''
+    Helper function for trainIters. Perform one iteration of training. Generate a random number, if the number is larger then the teacher
+    forcing ratio, then don't use teacher forcing. 
+    '''
     encoder_hidden = encoder.initHidden()
 
     encoder_optimizer.zero_grad()
@@ -203,25 +230,21 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
         encoder_outputs[ei] = encoder_output[0, 0]
 
     decoder_input = torch.tensor([[sos_tkn]], device=device)
-
     decoder_hidden = encoder_hidden
-
     use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
 
     if use_teacher_forcing:
-        # Teacher forcing: Feed the target as the next input
         for di in range(target_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
+            decoder_output, decoder_hidden, _ = decoder(
                 decoder_input, decoder_hidden, encoder_outputs)
             loss += criterion(decoder_output, target_tensor[di])
             decoder_input = target_tensor[di]  # Teacher forcing
 
     else:
-        # Without teacher forcing: use its own predictions as the next input
         for di in range(target_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
+            decoder_output, decoder_hidden, _ = decoder(
                 decoder_input, decoder_hidden, encoder_outputs)
-            topv, topi = decoder_output.topk(1)
+            _, topi = decoder_output.topk(1)
             decoder_input = topi.squeeze().detach()  # detach from history as input
 
             loss += criterion(decoder_output, target_tensor[di])
@@ -235,9 +258,10 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
 
     return loss.item() / target_length
 
-import time
-import math
 
+'''
+Helper functions for plotting time training took. 
+'''
 
 def asMinutes(s):
     m = math.floor(s / 60)
@@ -249,19 +273,23 @@ def timeSince(since):
     now = time.time()
     return asMinutes(now - since)
 
-def trainIters(encoder, decoder, n_iters, pairs, print_every=1000, plot_every=1, learning_rate=0.01):
-    start = time.time()
+def trainIters(encoder, decoder, n_iters, pairs, learning_rate=0.01):
+    '''
+    Train model. Print losses and time trianing took for each epoch. Plot losses every epoch. 
+    '''
+    start = time.time() #for tracking training time
     plot_losses = []
-    print_loss_total = 0  # Reset every print_every
-    plot_loss_total = 0  # Reset every plot_every
+    print_loss_total = 0 
+    plot_loss_total = 0  
 
     encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
     decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
-                      
     criterion = nn.NLLLoss()
+
+    #generate pairs and cast to tensor
     pairs = [tensorsFromPair(pair) for pair in pairs]
+
     #epoch
-    os.chdir('epochs')
     for i in range(n_iters//len(pairs)):
         #shuffle pairs
         np.random.shuffle(pairs)
@@ -271,36 +299,14 @@ def trainIters(encoder, decoder, n_iters, pairs, print_every=1000, plot_every=1,
             input_tensor = training_pair[0]
             target_tensor = training_pair[1]
 
-            loss = train(input_tensor, target_tensor, encoder,
-                        decoder, encoder_optimizer, decoder_optimizer, criterion)
+            loss = train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
             print_loss_total += loss
             plot_loss_total += loss
 
-            if (k+1) % 1000 == 0:
-                print('iter: %d/%d; %s; %.4f' % ((k+1), len(pairs), timeSince(start), print_loss_total / k))
-        
-        #make directory 
-        foldername = 'epoch_%d' % (i)
-        os.makedirs(foldername)
-
-        #change path
-        os.chdir(foldername)
-
-        #save models
-        encoder_name = 'encoder_model_epoch_%d.pt' % (i)
-        decoder_name = 'decoder_model_epoch_%d,pt' % (i)
-        torch.save(encoder1.state_dict(), encoder_name)
-        torch.save(attn_decoder1.state_dict(), decoder_name)
-
-        evaluatePredictionData(encoder1, attn_decoder1)
-
-        #return to parent directory 
-        os.chdir("..")
-
+      
         print_loss_avg = print_loss_total/len(pairs)
         print_loss_total = 0
-        print('%s (%d %d%%) %.4f' % (timeSince(start),
-                                    (k+1), (k+1) / n_iters * 100, print_loss_avg))
+        print('%s (%d %d%%) %.4f' % (timeSince(start), (k+1), (k+1) / n_iters * 100, print_loss_avg))
 
         plot_loss_avg = plot_loss_total/len(pairs)
         plot_losses.append(plot_loss_avg)
@@ -310,8 +316,10 @@ def trainIters(encoder, decoder, n_iters, pairs, print_every=1000, plot_every=1,
 
 
 
-
 def Plot(points):
+    '''
+    Function for plotting loss over an epoch
+    '''
     plt.figure()
     plt.plot(points)
     plt.savefig('loss.png')
@@ -323,55 +331,13 @@ def Plot(points):
 '''
 Evaluate 
 '''
-def evaluate(encoder, decoder, sentence, max_length=max_length):
-    with torch.no_grad():
-        input_tensor = tensorFromWord(source, sentence)
-        input_length = input_tensor.size()[0]
-        encoder_hidden = encoder.initHidden()
-
-        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
-
-        for ei in range(input_length):
-            encoder_output, encoder_hidden = encoder(input_tensor[ei],
-                                                     encoder_hidden)
-            encoder_outputs[ei] += encoder_output[0, 0]
-
-        decoder_input = torch.tensor([[sos_tkn]], device=device)  # SOS
-
-        decoder_hidden = encoder_hidden
-
-        decoded_words = []
-        decoder_attentions = torch.zeros(max_length, max_length)
-
-        for di in range(max_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
-                decoder_input, decoder_hidden, encoder_outputs)
-            decoder_attentions[di] = decoder_attention.data
-            topv, topi = decoder_output.data.topk(1)
-            if topi.item() == eos_tkn:
-                decoded_words.append('<EOS>')
-                break
-            else:
-                decoded_words.append(target.index2letter[topi.item()])
-
-            decoder_input = topi.squeeze().detach()
-
-        return decoded_words, decoder_attentions[:di + 1]
-
-def evaluateRandomly(encoder, decoder, n=10):
-    for i in range(n):
-        pair = random.choice(pairs)
-        print('>', pair[0])
-        print('=', pair[1])
-        output_letters, attentions = evaluate(encoder, decoder, pair[0])
-        output_word = ''.join(output_letters[:-1])
-        #output_word = 'C'+output_word+'F'
-        print('<', output_word)
-        print('')
 
 def evalOnPred(encoder, decoder, word, max_length=max_length):
-     with torch.no_grad(): 
-        input_tensor = tensorFromWord(predict, word)
+    '''
+    Helper function for evaluatePredictionData. 
+    '''
+    with torch.no_grad(): 
+        input_tensor = tensorFromSequence(predict, word)
         input_length = input_tensor.size()[0]
         encoder_hidden = encoder.initHidden()
 
@@ -393,7 +359,7 @@ def evalOnPred(encoder, decoder, word, max_length=max_length):
             decoder_output, decoder_hidden, decoder_attention = decoder(
                 decoder_input, decoder_hidden, encoder_outputs)
             decoder_attentions[di] = decoder_attention.data
-            topv, topi = decoder_output.data.topk(1)
+            _, topi = decoder_output.data.topk(1)
             if topi.item() == eos_tkn:
                 decoded_words.append('<EOS>')
                 break
@@ -405,6 +371,9 @@ def evalOnPred(encoder, decoder, word, max_length=max_length):
         return decoded_words, decoder_attentions[:di + 1]
 
 def evaluatePredictionData(encoder, decoder):
+    '''
+    Generate and write predictions from data_predict.txt to output file 
+    '''
     filename = 'result_predict.txt'
     with open(filename, 'w') as f:
         for i in range(len(predictInput)):
@@ -417,8 +386,30 @@ def evaluatePredictionData(encoder, decoder):
 
 hidden_size = 256
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-encoder1 = Encoder(source.n_letters, hidden_size).to(device)
-attn_decoder1 = AttnDecoder(hidden_size, target.n_letters, dropout_p=0.1).to(device)
+encoder1 = Encoder(22, hidden_size).to(device)
+decoder = Decoder(hidden_size, 22, dropout_p=0.1).to(device)
 
-trainIters(encoder1, attn_decoder1, 902224 , pairs, print_every=1)
+if Train:
+    trainIters(encoder1, decoder, 902224 , pairs)
+    predict = loadPredict(predictInput)
+else:
+    #need to load target object for evalOnPred function
+    urllib.request.urlretrieve('https://drive.google.com/u/0/uc?id=1dJmmnPYyvMmWQENO9hIx2KQ_ELSh51Gv&export=download', 'target.obj')
+    f = open('target.obj', 'rb')
+    target = pickle.load(f)
+    f.close()
 
+    #load trained models from google drive
+    print('downloading trained models....')
+    urllib.request.urlretrieve('https://drive.google.com/u/0/uc?id=1c39Nc1dmOvxYDDrJRjdLzjZSvxJRgF_5&export=download', 'encoder.pt')
+    encoder1.load_state_dict(torch.load('encoder.pt', map_location=device))
+    encoder1.eval()
+
+    urllib.request.urlretrieve('https://drive.google.com/u/0/uc?id=1PGbmccH2JV1wKUoDkBnMv42IMjFVlA2T&export=download', 'decoder.pt')
+    decoder.load_state_dict(torch.load('decoder.pt', map_location=device))
+    decoder.eval()
+
+    print('models downloaded, generating predictions...')
+    evaluatePredictionData(encoder1, decoder)
+
+    print('generated predictions!')
